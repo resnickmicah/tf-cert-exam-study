a46fcf1a628d96e89ccc7128e29fb3a6cf9ade29
Micah Resnick <resnickmicah@gmail.com>
2025-03-16 20:47:21 -0400

    Finish expressions tutorial
    https://developer.hashicorp.com/terraform/tutorials/certification-associate-tutorials-003/expressions

--- expressions/variables.tf
+++ expressions/variables.tf
@@ -31,3 +31,8 @@ variable "team" {
   default     = "hashicorp"
 }
 
+variable "high_availability" {
+  type        = bool
+  description = "If this is a multiple instance deployment, choose `true` to deploy 3 instances"
+  default     = true
+}
