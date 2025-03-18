8f8bf953a828e17a48a49a7e122fbe764f08e951
Micah Resnick <resnickmicah@gmail.com>
2025-03-18 17:54:30 -0400

    Finish state tutorial
    https://developer.hashicorp.com/terraform/tutorials/certification-associate-tutorials-003/state-cli

--- state/main.tf
+++ state/main.tf
@@ -35,7 +35,17 @@ resource "aws_security_group" "sg_8080" {
   }
 }
 
-resource "aws_instance" "example" {
+/*
+removed {
+  from = aws_instance.example_new
+
+  lifecycle {
+    destroy = false
+  }
+}
+*/
+
+resource "aws_instance" "example_new" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg_8080.id]
--- state/outputs.tf
+++ state/outputs.tf
@@ -1,10 +1,10 @@
 output "instance_id" {
-  value       = aws_instance.example.id
+  value       = aws_instance.example_new.id
   description = "The ID of the AWS instance"
 }
 
 output "public_ip" {
-  value       = aws_instance.example.public_ip
+  value       = aws_instance.example_new.public_ip
   description = "The public IP of the web server"
 }
 
